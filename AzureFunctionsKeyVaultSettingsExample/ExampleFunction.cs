8820e28c9cf74e91cd138f22e6de6a2a400d99d0
Yadagani Sudhakar <Yadagani.Sudhakar@vca.com>
2020-04-24 15:02:57 +0530

    updated with CosmosDBTrigger: explanation given by 'briandunnington.github.io/azure_functions_dynamic_connection_string'

--- AzureFunctionsKeyVaultSettingsExample/ExampleFunction.cs
+++ AzureFunctionsKeyVaultSettingsExample/ExampleFunction.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.Documents;
 using Microsoft.Extensions.Logging;
 
 namespace AzureFunctionsKeyVaultSettingsExample
@@ -18,12 +20,35 @@ namespace AzureFunctionsKeyVaultSettingsExample
          * QueueName are both settings from Key Vault.
          * */
 
-        [FunctionName("ProcessQueueItem")]
-        public void ProcessQueueItem([QueueTrigger("%QueueName%", Connection = "StorageAccountConnectionString")]string myQueueItem, ILogger log)
+        //[FunctionName("ProcessQueueItem")]
+        //public void ProcessQueueItem([QueueTrigger("%QueueName%", Connection = "saCosmosDocumentDbConnectionString")]string myQueueItem, ILogger log)
+        //{
+        //    log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        //    var val = sampleService.GetValue();
+        //    log.LogInformation($"The value from the SampleService is: {val}");
+        //}
+        [FunctionName("ItemChangeFeedListener")]
+        public static void Run([CosmosDBTrigger(
+            databaseName: "ItemDB",
+            collectionName: "Item",
+            LeaseCollectionName = "leases",
+            LeaseCollectionPrefix = "ItemFeed",
+           // ConnectionStringSetting = "saCosmosDbConnString",
+             CreateLeaseCollectionIfNotExists = true)] IReadOnlyList<Document> input, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
-            var val = sampleService.GetValue();
-            log.LogInformation($"The value from the SampleService is: {val}");
+            try
+            {
+                if (input != null && input.Count > 0)
+                {
+                    log.LogInformation("Documents modified " + input.Count);
+                    log.LogInformation("First document Id " + input[0].Id);
+                    throw new Exception("is checkpoint changed to next one OR is the change feed again send the old document!");
+                    
+                }
+            }
+            catch (Exception ex) {
+                log.LogError("Exception thrown for testing purpose of checkpoint : " + ex.Message);
+            }
         }
     }
 }
--- AzureFunctionsKeyVaultSettingsExample/Startup.cs
+++ AzureFunctionsKeyVaultSettingsExample/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using AzureFunctionsKeyVaultSettingsExample;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -13,6 +14,11 @@ namespace AzureFunctionsKeyVaultSettingsExample
             // Pass in the name of the setting that contains your Key Vault url 
             // (setting will be read from local.settings.json or Azure app settings)
             var config = builder.AddAzureKeyVaultConfiguration("KeyVaultEndpoint");
+
+            builder.Services.PostConfigure<CosmosDBOptions>(options =>
+            {
+                options.ConnectionString = config["saCosmosDbConnString"]; // saCosmosDbConnString is a secret in the Key Vault
+            });
 
             // Now config contains all secrets from the Key Vault and you can use them in your DI setup
             builder.Services.AddSingleton<ISampleService>(p => new SampleService(config["SampleValue"]));
